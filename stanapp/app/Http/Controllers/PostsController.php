e5e5ee588bcbfb837f077fe34d804b5fcb31c7f9
anael <anaellatassa@gmail.com>
2019-01-19 23:23:00 +0100

    Debut d'ajout d'un formulaire

--- stanapp/app/Http/Controllers/PostsController.php
+++ stanapp/app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
@@ -13,7 +14,11 @@ class PostsController extends Controller
      */
     public function index()
     {
-        //
+        //$posts = Post::all();
+        //$posts = Post::orderBy('title','desc')->get();
+
+        $posts = Post::orderBy('title','desc')->paginate(10);
+        return view('posts.index')->with('posts',$posts);
     }
 
     /**
@@ -23,7 +28,7 @@ class PostsController extends Controller
      */
     public function create()
     {
-        //
+        return view('posts.create');
     }
 
     /**
@@ -45,7 +50,8 @@ class PostsController extends Controller
      */
     public function show($id)
     {
-        //
+        $post =  Post::find($id);
+        return view('posts.show')->with('post',$post);
     }
 
     /**
